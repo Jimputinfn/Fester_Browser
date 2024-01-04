f02adb08a670c1e499cf23e58f1aa85638af7331
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-04 02:12:35 +0200

    Add files via upload

--- Browser/BrowserConfig.cs
+++ Browser/BrowserConfig.cs
@@ -13,12 +13,12 @@ namespace SharpBrowser.Browser
 
         public static string Branding = "FesterBrowser";
         public static string AcceptLanguage = "en-US,en;q=0.9";
-        public static string UserAgent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36" + Cef.CefSharpVersion; // UserAgent to fix issue with Google account authentication		
+        public static string UserAgent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" + Cef.CefSharpVersion; // UserAgent to fix issue with Google account authentication		
         public static string HomepageURL = "https://www.startpage.com/";
         public static string NewTabURL = "https://www.startpage.com/";
         public static string InternalURL = "festerbrowser";
         public static string DownloadsURL = "festerbrowser://storage/downloads.html";
-        public static string UpdateCenter = "https://pastebin.com/raw/8CXAreig";
+        public static string UpdateCenter = "https://pastebin.com/raw/iSJJnvz5";
         public static string FileNotFoundURL = "festerbrowser://storage/errors/notFound.html";
         public static string CannotConnectURL = "festerbrowser://storage/errors/cannotConnect.html";
         public static string SearchURL = "https:/www.startpage.com/do/search?query=";
